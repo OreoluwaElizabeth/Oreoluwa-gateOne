f6b9ae822ddcdc5e05088cd9def57739fa2b3b96
OreoluwaElizabeth <oreoluwaadeyemo77@gmail.com>
2024-05-14 13:00:34 -0800

    updated file

--- DaySeven/phoneBook/PhoneBook.java
+++ DaySeven/phoneBook/PhoneBook.java
@@ -13,95 +13,119 @@ public class PhoneBook {
 
    ArrayList<String>phoneNumbers = new ArrayList<String>();
 
-   String option = """
-
-   Welcome to Phone Book
-
-   Please select an option below
-
-   ==============================
-
-   1 -> Add Contact
-
-   2 -> Remove / Delete Contact
-
-   3 -> Find Contact By Phone Number
-
-   4 -> Find Contact By First Name
-
-   5 -> Find Contact By Last Name
-
-   6 -> Edit Contact
-
-   """;
-
-   System.out.println(option);
-
-   int userChoice = scanner.nextInt();
-
-   switch (userChoice) {
-
-   case 1: 
-
-   System.out.println("Add Contact");
+   int userChoice;
 
    do {
 
-     System.out.print("Enter first name: ");
-     String firstName = input.nextLine();
+      String option = """
 
-     System.out.print("Enter last name: ");
-     String lastName = input.nextLine();
+             Welcome to Phone Book
+
+             Please select an option below
+
+             ==============================
+
+             1 -> Add Contact
+
+             2 -> Remove / Delete Contact
+
+             3 -> Find Contact By Phone Number
+
+             4 -> Find Contact By First Name
+
+             5 -> Find Contact By Last Name
+
+             6 -> Edit Contact
+
+             7 -> Exit
+
+             """;
+
+      System.out.println(option);
+
+      userChoice = scanner.nextInt();
+
+      switch (userChoice) {
+
+          case 1: 
+
+              System.out.println("Add Contact");
+
+              do {
+
+                System.out.print("Enter first name: ");
+                String firstName = input.nextLine();
+
+                System.out.print("Enter last name: ");
+                String lastName = input.nextLine();
    
-     String fullName = firstName + " " + lastName;
-     names.add(fullName);
+                String fullName = firstName + " " + lastName;
+                names.add(fullName);
 
-     System.out.print("Enter phone number: ");
-     String phoneNumber = input.nextLine();
-     phoneNumbers.add(phoneNumber);
+                System.out.print("Enter phone number: ");
+                String phoneNumber = input.nextLine();
+                phoneNumbers.add(phoneNumber);
 
-     System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>");
+                System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>");
 
-     System.out.println("Saved Successfully!!");
+                System.out.println("Saved Successfully!!");
 
-     System.out.print("Do you want to save another contact? (yes/no): ");
-    } while (input.nextLine().equalsIgnoreCase("yes"));
+                System.out.print("Do you want to save another contact? (yes/no): ");
+            } while (input.nextLine().equalsIgnoreCase("yes"));
 
-     System.out.println("Thank you for using phone book");
+            System.out.println("Thank you for using phone book");
 
-     System.out.println(names);
+            System.out.println(names);
 
-     System.out.println(phoneNumbers);
+            System.out.println(phoneNumbers);
 
-     break;
+            break;
 
-  default:
-     System.out.println("Invalid choice");
-     break;
-     
+  
 
-  case 2:
+      case 2:
 
-     System.out.println("Remove / Delete Contact");
+           System.out.println("Remove / Delete Contact");
 
-     System.out.println("Lists of contacts saved");
+           System.out.println("Lists of contacts saved");
 
-     for (int counter = 0; counter < names.size(); counter++) {
+           for (int counter = 0; counter < names.size(); counter++) {
 
-          System.out.println((counter + 1) + " , "  + names.get(counter) + " -> " + phoneNumbers.get(counter));
+                System.out.println((counter + 1) + " "  + names.get(counter) + " -> " + phoneNumbers.get(counter));
 
-             }
+            }
 
-     System.out.print("Enter the contact name you want to remove / delete: ");
-     String removeContact = input.nextLine();
+            System.out.print("Enter the contact name you want to remove / delete: ");
+            String removeContact = input.nextLine();
 
-     break;
-       }
+            int index = -1;
+            for (int count = 0; count < names.size(); count++) {
+               if (names.get(count).equalsIgnoreCase(removeContact)) {
+                    index = count;
+                    break;
+                 }
+               }
 
-        
+             if (index != -1) {
+               names.remove(removeContact);
+               phoneNumbers.remove(removeContact);
+               System.out.println("Deleting >>>>>>>>>>>>>>>>>>>>>>>>>");
+               System.out.println("Contact Deleted sucessfully!!!");
+                }
+            else {
+                System.out.println("No contact found.");
+              }
+            break;
 
+      default:
+          if (userChoice != 7)
+              System.out.println("Invalid choice");
+          break;
 
- }
+     }
+  } while (userChoice != 7);
+
+}
 
 
 }
